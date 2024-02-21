be1d87632e6aad2503615fa764540ecc39d36010
ianvictor <ianvictorandrade@hotmail.com>
2024-02-21 11:00:53 -0400

    initial commit

---
+++ back/src/main/java/ifro/hackatonads/HackatonAdsApplication.java
@@ -0,0 +1,13 @@
+package ifro.hackatonads;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HackatonAdsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HackatonAdsApplication.class, args);
+	}
+
+}
