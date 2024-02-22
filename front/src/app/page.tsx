e551ddbc4c1da70d1ca955a97c2efdc900bee5f7
ianvictor <ianvictorandrade@hotmail.com>
2024-02-22 12:36:01 -0400

    next 14

--- front/src/app/page.tsx
+++ front/src/app/page.tsx
@@ -3,7 +3,7 @@ import TelaInicial from "@/components/inicial";
 export default function Home() {
   return (
       <div className="flex flex-col justify-center items-center gap-4 bg-zinc-700 h-screen w-screen">
-            <TelaInicial />
+        <TelaInicial />
       </div>
   )
 }
--- front/src/components/inicial.tsx
+++ front/src/components/inicial.tsx
@@ -6,13 +6,13 @@ export default function TelaInicial() {
             <input type="text" placeholder="E-mail" className="text-black p-2 w-[20rem] h-[3rem] rounded-lg"/>
             <input type="text" placeholder="Senha" className="text-black p-2 w-[20rem] h-[3rem] rounded-lg"/>
             <button className="bg-blue-700 p-3 w-[10rem] rounded-lg hover:bg-blue-800"
-                onClick={() => {
-                    alert('Bem vindo, Professor(a)!'
-                    )
-                }}
+                    onClick={() => {
+                        alert('Bem vindo, Professor(a)!'
+                        )
+                    }}
             >
                 Entrar
             </button>
         </>
-)
+    )
 }
