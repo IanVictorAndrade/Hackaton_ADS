7964cd1ef06de50e79a49f4519e0fcc19e9809c2
Maria-Eduarda90 <snowdennaruto90@gmail.com>
2024-02-24 15:10:41 -0300

    first commit

--- front/src/app/page.tsx
+++ front/src/app/page.tsx
@@ -1,9 +1,7 @@
-import TelaInicial from "@/components/inicial";
-
 export default function Home() {
   return (
-      <div className="flex flex-col justify-center items-center gap-4 bg-zinc-700 h-screen w-screen">
-        <TelaInicial />
-      </div>
+    <div className="">
+      teste
+    </div>
   )
 }
