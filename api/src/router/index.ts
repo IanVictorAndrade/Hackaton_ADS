de8612036376a3b63da4c867f949d5bde9a5e217
Maria-Eduarda90 <snowdennaruto90@gmail.com>
2024-02-25 20:24:46 -0300

    feat: criação da rota pra criar professores

--- api/src/router/index.ts
+++ api/src/router/index.ts
@@ -1,10 +1,14 @@
 import { Router } from "express";
 import { LoginAdminController } from "../controllers/admin/loginAdmin.controller";
+import { ensureAuthenticated } from "../middlewares/ensureAuthenticated";
+import { CreateProfessorController } from "../controllers/admin/createProfessor.controller";
 
 const router = Router();
 
 const adminLogin = new LoginAdminController();
+const professor = new CreateProfessorController();
 
 router.post("/api/adimin/login", adminLogin.handlelogin);
+router.post("/api/adimin/create", ensureAuthenticated, professor.handle);
 
 export { router };
