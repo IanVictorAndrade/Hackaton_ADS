193a65d0a64bbb7a560eb3793ed609613341e604
Maria-Eduarda90 <snowdennaruto90@gmail.com>
2024-02-25 21:30:09 -0300

    feat: rota pra listar professores

--- api/src/router/index.ts
+++ api/src/router/index.ts
@@ -2,13 +2,16 @@ import { Router } from "express";
 import { LoginAdminController } from "../controllers/admin/loginAdmin.controller";
 import { ensureAuthenticated } from "../middlewares/ensureAuthenticated";
 import { CreateProfessorController } from "../controllers/admin/createProfessor.controller";
+import { ListarProfessorController } from "../controllers/admin/listarProfessor.controller";
 
 const router = Router();
 
 const adminLogin = new LoginAdminController();
 const professor = new CreateProfessorController();
+const listarProfessor = new ListarProfessorController();
 
 router.post("/api/adimin/login", adminLogin.handlelogin);
 router.post("/api/adimin/create", ensureAuthenticated, professor.handle);
+router.get("/api/adimin/list", ensureAuthenticated, listarProfessor.handle);
 
 export { router };
